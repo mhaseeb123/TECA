d92c5293b6b5c3304459b1360cf90f56b11c8bc8
elbashandy <a.elbashandy@gmail.com>
2020-07-10 10:51:49 -0700

    Refactored the app tests to have the app options being specified in their respective bash scripts, following the bayesian app test design

--- test/apps/test_bayesian_ar_detect_app.sh
+++ test/apps/test_bayesian_ar_detect_app.sh
@@ -2,7 +2,7 @@
 
 if [[ $# < 2 ]]
 then
-    echo "usage: test_bayesian_ar_detect_app.sh [app prefix] " \
+    echo "usage: test_bayesian_ar_detect_app.sh [app prefix] "   \
          "[data root] [mpi exec] [test cores]"
     exit -1
 fi
@@ -10,25 +10,26 @@ fi
 app_prefix=${1}
 data_root=${2}
 
-launcher=
 if [[ $# -eq 4 ]]
 then
     mpi_exec=${3}
     test_cores=${4}
     launcher="${mpi_exec} -n ${test_cores}"
+    steps_per_file="--steps_per_file 4"
 fi
 
 set -x
 
 # run the app
-${launcher} ${app_prefix}/teca_bayesian_ar_detect                           \
-    --verbose --input_regex "${data_root}/ARTMIP_MERRA_2D_2017-05.*\.nc$"   \
+${launcher} ${app_prefix}/teca_bayesian_ar_detect                \
+    --input_regex "${data_root}/ARTMIP_MERRA_2D_2017-05.*\.nc$"  \
+    --verbose ${steps_per_file}                                  \
     --output_file test_bayesian_ar_detect_app_output.nc
 
 # run the diff
-${app_prefix}/teca_cartesian_mesh_diff                  \
-    0 test_bayesian_ar_detect_app_output.nc             \
+${app_prefix}/teca_cartesian_mesh_diff                           \
+    "test_bayesian_ar_detect_app_output.*\.nc"                   \
     "${data_root}/test_bayesian_ar_detect_app_ref.bin"
 
 # clean up
-rm test_bayesian_ar_detect_app_output.nc
+rm test_bayesian_ar_detect_app_output*.nc
